939730d97c35758af8520d1aa4e01eeb1c2406e9
Rakibul Alam <rakibulalam59900@gmail.com>
2026-01-23 23:23:56 +0600

    fix user profile

--- controller/profileController.js
+++ controller/profileController.js
@@ -2,7 +2,12 @@ const User=require('../model/User');
 async function getProfile(req,res){
     try {
         userProfile=await User.findById(req.user.userid).exec();
-        res.status(200).json({message:"user Profile",details:userProfile});
+        const result={
+            name:userProfile.name,
+            email:userProfile.email,
+            role:userProfile.role
+        }
+        res.status(200).json({message:"user Profile",details:result});
     } catch (error) {
         res.status(404).json({message:"User not found"});
     }
